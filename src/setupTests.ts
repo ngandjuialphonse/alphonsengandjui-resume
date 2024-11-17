e977390b434f9b5ab24f57cd7fe0484dd5336d00
ngandjuialphonse <ngandjuialphonse@gmail.com>
2024-11-17 02:28:11 -0600

    resume

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,12 @@
 // src/setupTests.ts
 import '@testing-library/jest-dom';
+// setupTests.ts
+beforeAll(() => {
+  window.matchMedia = window.matchMedia || function() {
+    return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {},
+    };
+  };
+});

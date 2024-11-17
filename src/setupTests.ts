816cab2ae11cbf363169581ff28aa8cdbb9635f0
ngandjuialphonse <ngandjuialphonse@gmail.com>
2024-11-17 02:35:23 -0600

    resume

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,10 +1,24 @@
-// setupTests.ts
+// src/setupTests.ts
+import '@testing-library/jest-dom';
+
 beforeAll(() => {
   window.matchMedia = window.matchMedia || function() {
     return {
       matches: false,
       addListener: function() {},
       removeListener: function() {},
+      addEventListener: function() {},
+      removeEventListener: function() {},
+      dispatchEvent: function() { return false; },
     };
   };
 });
+
+// Extend expect matchers
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeInTheDocument(): R;
+    }
+  }
+}
